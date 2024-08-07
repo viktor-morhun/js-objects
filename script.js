d7d0f5717084d8856d782d0d528e57dacc042ed0
Viktor Morhun <morhun.fe@gmail.com>
2024-08-07 15:28:10 +0300

    upgrade carObj methods, add eventListeners

--- script.js
+++ script.js
@@ -1,36 +1,68 @@
 'use strict';
 
 //Task 1
-
 let carObj = {
-  brand: 'Tesla',
-  model: '3',
-  year: 2019,
+  brand: 'DeLorean',
+  model: 'DMC-12',
+  year: 1982,
   speed: 100,
-  fuelTank: 80,
-  fuelConsumption: 15,
-  drivers: ['Deen', 'Sam'],
+  fuelTank: 52,
+  fuelConsumption: 9.8,
+  drivers: ['Marty McFly', 'Doc Brown', 'Rick Sanchez', 'Morty Smith'],
+
   getInfo() {
-    console.log(carObj);
+    const resultElement = document.getElementById('car-result');
+
+    resultElement.innerHTML = 
+    `Car: ${this.brand} model ${this.model}, ${this.year} year,
+     avg speed: ${this.speed}km/h, fuel capacity: ${this.fuelTank}l,
+      fuel consumption per 100km: ${this.fuelConsumption}l.\nDrivers: ${this.drivers.join(', ')}.`;
   },
-  addDriver(name) {
-    this.drivers.push(name);
+  addDriver() {
+    const driverElement = document.getElementById('driver-name');
+
+    this.drivers.push(driverElement.value.trim());
+    if (document.getElementById('car-result').innerHTML){
+      this.getInfo();
+    } else {
+      document.getElementById('car-result').innerHTML = `Driver ${driverElement.value} added.`
+    }
+
+    driverElement.value = '';
   },
-  checkAcces(name) {
-    return this.drivers.includes(name)
+  checkAcces() {
+    const driversNameElement = document.getElementById('check-driver');
+    const resultElement = document.getElementById('car-result');
+    const driverCheck = this.drivers.includes(driversNameElement.value.trim()) 
+    ? `Yes, ${driversNameElement.value} has acces.` 
+    : `No, ${driversNameElement.value} hasn't acces.`;
+
+    resultElement.innerHTML = driverCheck;
   },
-  calculatePath(distance) {
-    let travelTime = distance/this.speed + Math.floor(distance/this.speed/4);
-    let fuel = distance/100 * this.fuelConsumption;
-    return `Estimated time: ${travelTime.toFixed(2)}h, fuel: ${fuel}`;
+  calculateTrip() {
+    const distance = document.getElementById('distance').value;
+    const resultElement = document.getElementById('car-result');
+    const fuel = distance/100 * this.fuelConsumption;
+    const travelTime = Math.round((distance/this.speed + Math.floor(distance/this.speed/4)) * 60);
+    const timeHours = Math.floor(travelTime / 60);
+    const timeMinutes = travelTime % 60;
+    
+
+    resultElement.innerHTML = `Estimated time: ${timeHours}hours ${timeMinutes}minutes, fuel: ${fuel.toFixed(2)}liters`;
   },
 }
 
-carObj.addDriver('Bruh');
+const carInfoElement = document.getElementById('car-info');
+carInfoElement.addEventListener('click', () => carObj.getInfo());
 
-carObj.getInfo();
+const addDriverElement = document.getElementById('add-driver');
+addDriverElement.addEventListener('click', () => carObj.addDriver());
 
-console.log(carObj.checkAcces('Bruh'));
-console.log(carObj.calculatePath(600));
+const checkDriverElement = document.getElementById('check-driver-button');
+checkDriverElement.addEventListener('click', () => carObj.checkAcces());
 
-document.getElementById('car-info').addEventListener('click', carObj.getInfo);
+const calculateTripElement = document.getElementById('calculate-trip');
+calculateTripElement.addEventListener('click', () => carObj.calculateTrip())
+
+
+//Task 2
